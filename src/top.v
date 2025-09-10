9455bf2ca288d661b4bc57e8228509e4b3fa650a
Julia Desmazes - ws <julia.desmazes@gmail.com>
2025-09-09 18:46:32 -0700

    Better leds, and maybe a clock this time

--- src/top.v
+++ src/top.v
@@ -4,7 +4,27 @@ module top (
 
 	output wire [3:0] Led_o 
 );
+    wire        clk_ibuf;
+	reg  [28:0] ctr_q; 
+	reg         unused_ctr_q;
 
-assign Led_o = 4'b1010;
 
+    IBUFDS #(
+        .DIFF_TERM("TRUE"),
+        .IOSTANDARD("LVDS")
+    ) m_ibufds (
+        .I(Clk_100mhz_p_i),
+        .IB(Clk_100mhz_n_i),
+        .O(clk_ibuf)
+    );
+
+    BUFG m_bufg (
+        .I(clk_ibuf),
+        .O(clk)
+    );
+
+	always @(posedge clk)
+		{ unused_ctr_q, ctr_q } <= ctr_q + 29'b1;	
+	
+	assign Led_o = ctr_q[28:25];
 endmodule
